8b358e437ad6ac62b8801c42b8f32c9c4f15aedf
nayfusaurus <35911760+nayfusaurus@users.noreply.github.com>
2025-05-26 20:18:37 +0800

    updated player controls

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -9,11 +9,22 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT/2)
+
     while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
         pygame.Surface.fill(screen, (0,0,0))
-
-
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.get_time() / 1000
 
 if __name__ == "__main__":
     main()
